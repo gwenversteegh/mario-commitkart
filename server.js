8bbf692a4c5e24550a5553be8f93c4115ff3ce8f
Sophie van der Burg <sophievdburg9@gmail.com>
2022-04-11 14:25:30 +0200

    data inladen werkt!!!!!!!! :)

--- server.js
+++ server.js
@@ -3,6 +3,13 @@ const express = require('express')
 const app = express()
 const port = 3333
 
+require('dotenv').config({path: '.env'})
+
+const { graphql } = require('@octokit/graphql')
+const graphqlAuth = graphql.defaults({
+  headers: { authorization: 'token ' + process.env.API_KEY },
+})
+
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
@@ -12,9 +19,47 @@ app.get("/", renderPagina)
 
 /* Functies */
 function renderPagina( req, res){
-    res.render('index')
+    graphqlAuth(`query {
+        repositoryOwner(login: "cmda-minor-web") {
+          repository(name: "project-2-2122") {
+            forks(first: 100) {
+              edges {
+                node {
+                  owner {
+                    avatarUrl
+                    login
+                    ... on User {
+                      url
+                    }
+                  }
+                  defaultBranchRef {
+              target {
+                ... on Commit {
+                  id
+                  history {
+                    totalCount
+                  }
+                }
+              }
+            }
+                }
+              }
+            }
+            
+          }
+        }
+      }`)
+    .then((data) => {
+        res.render('index', {
+          data: data.repositoryOwner.repository.forks.edges,
+        })
+    })
 }
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
   })
+
+
+
+  
